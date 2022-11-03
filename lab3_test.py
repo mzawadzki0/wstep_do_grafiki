435d053c847ed4879de474948e7eeed0a9aca45a
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2022-11-03 10:48:00 +0100

    test grayscale sepia

--- lab3_test.py
+++ lab3_test.py
@@ -3,8 +3,8 @@ from lab3 import Image
 img = Image('lena.jpg')
 
 # grayscale
-# gray = img.to_gray(high_contrast=True)
-# gray.show_img()
+gray = img.to_gray(high_contrast=True)
+gray.show_img()
 
 sepia = img.to_sepia(alpha_beta=(1.5, 0.5))
 sepia.show_img()
