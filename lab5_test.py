1b6cb0df00481c61aa15925f51152e0564c88fe5
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2022-12-22 12:21:51 +0100

    usunięte printy do debugowania

--- lab5_test.py
+++ lab5_test.py
@@ -5,12 +5,10 @@ lena = Image('lena.jpg')
 
 # lena to grayscale
 lena.data, lena.color_model = lena.to_gray().data, lena.to_gray().color_model
-print(lena.data)
 
 # lena to cumulated histogram
 lena.histogram().plot()
 lena.histogram().to_cumulated().plot()
-# print( lena.histogram().to_cumulated().values)
 
 # lena aligned
 lena.data = lena.align_image().data
